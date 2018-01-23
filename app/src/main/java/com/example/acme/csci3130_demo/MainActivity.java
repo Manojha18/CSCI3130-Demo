cdda8a96479762cb2cce9d71a614b4a2f7b77d8f
Manojha18 <35583244+Manojha18@users.noreply.github.com>
2018-01-23 13:31:12 -0400

    Assignment1_question3
    Added UIElements

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button sampleButton;
+    EditText sampleEdit;
+    TextView sampleText;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sampleButton = (Button) findViewById(R.id.buttonNew);
+        sampleEdit = (EditText) findViewById(R.id.editText);
+        sampleText = (TextView) findViewById(R.id.textView);
+
+        sampleButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = sampleEdit.getText().toString();
+                sampleText.setText(result);
+            }
+        });
     }
 }
